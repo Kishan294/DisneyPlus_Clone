5cf02ff302421c198331f359e557be04e0377556
Kishan Kumar <20bc294@srcc.edu>
2022-10-14 14:30:44 +0530

    Env update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBRReYI36_3jBMcH0qRUtIlp224_HONzY",
-  authDomain: "disney-clone-8da67.firebaseapp.com",
-  databaseURL: "https://disney-clone-8da67-default-rtdb.firebaseio.com",
-  projectId: "disney-clone-8da67",
-  storageBucket: "disney-clone-8da67.appspot.com",
-  messagingSenderId: "393649994821",
-  appId: "1:393649994821:web:8587d44fcc713f20b0b72c",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = getApps.length > 0 ? getApp : initializeApp(firebaseConfig);
